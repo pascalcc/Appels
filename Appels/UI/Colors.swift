eb53a2a4a47715bb905dfbe1536dde200695da99
PascalCC <costacunha@gmail.com>
2024-11-09 15:11:29 +0100

    WIP UI

---
+++ Appels/UI/Colors.swift
@@ -0,0 +1,26 @@
+//
+//  Colors.swift
+//  Appels
+//
+//  Created by Pascal Costa-Cunha on 09/11/2024.
+//
+
+
+import UIKit
+
+struct Colors {
+
+    
+    
+    struct SettingsButton {
+        static let enabledBackground = UIColor(named: "MainColor")!
+        static let disabledBackground = UIColor.clear
+        static let disabledBorder = UIColor(red: 188.0/255.0, green: 197.0/255.0, blue: 200.0/255.0, alpha: 1)
+        
+        static let enabledFont = UIColor(red: 19.0/255.0, green: 56.0/255.0, blue: 59.0/255.0, alpha: 1)
+        static let disabledFont = UIColor(red: 97.0/255.0, green: 109.0/255.0, blue: 112.0/255.0, alpha: 1)
+    }
+    
+    
+    
+}
---
+++ Appels/UI/SettingButton.swift
@@ -0,0 +1,40 @@
+//
+//  CustomButton.swift
+//  Appels
+//
+//  Created by Pascal Costa-Cunha on 09/11/2024.
+//
+
+import UIKit
+
+@IBDesignable class SettingButton: UIButton {
+        
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+                        
+        layer.addSublayer(border)
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+                
+        setTitleColor(Colors.SettingsButton.disabledFont, for: .disabled)
+        setTitleColor(Colors.SettingsButton.enabledFont, for: .normal)
+    }
+    
+    private let border = CAShapeLayer()
+    
+    override func draw(_ rect: CGRect) {
+        border.lineWidth = 1.0
+        border.frame = bounds
+        border.fillColor = (isEnabled ? Colors.SettingsButton.enabledBackground : Colors.SettingsButton.disabledBackground).cgColor
+        border.strokeColor = (isEnabled ? .clear : Colors.SettingsButton.disabledBorder).cgColor
+        border.path = UIBezierPath(roundedRect: bounds, cornerRadius: 10).cgPath
+    }
+    
+    @IBAction func onPress(_ sender:SettingButton) {
+        isEnabled = true
+        sender.isEnabled = false
+    }
+    
+}
+
+
