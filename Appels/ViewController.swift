a80ba751cb32b2fa5bde810c2e100c9e56b63b43
PascalCC <costacunha@gmail.com>
2024-11-09 10:00:40 +0100

    Initial Commit

---
+++ Appels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appels
+//
+//  Created by Pascal Costa-Cunha on 09/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppelsTests/AppelsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppelsTests.swift
+//  AppelsTests
+//
+//  Created by Pascal Costa-Cunha on 09/11/2024.
+//
+
+import Testing
+@testable import Appels
+
+struct AppelsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
